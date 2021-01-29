9c8d49ad55115341a49789ec606d78632f4e24ce
zinirun <heojj97@naver.com>
2021-01-29 14:41:39 +0900

    Update select.go

--- go-routines/select.go
+++ go-routines/select.go
@@ -1,9 +1,9 @@
 package goroutines
 
 import (
-	"fmt"	"time"
+	"fmt"
+	"time"
 )
-
 
 func selectExample(c chan rune) {
 	// 채널에 값이 있으면 받고, 없으면 스킵하는 흐름
@@ -16,13 +16,14 @@ func selectExample(c chan rune) {
 }
 
 func selectTimerExample(recv chan rune) {
+	send := make(chan rune)
 	// 채널과의 통신을 기다리되 일정 시간동안만 대기 -> time.After 사용
 	select {
 	case n := <-recv:
 		fmt.Println(n)
 	case send <- 1:
 		fmt.Println("sent 1")
-	case <- time.After(5 * time.Second):
+	case <-time.After(5 * time.Second):
 		fmt.Println("No data for 5 seconds")
 		return
 	}
